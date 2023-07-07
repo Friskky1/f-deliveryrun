93464abdb58e97a9816263cb743d61549a251454
Friskky <brendanco7@hotmail.com>
2023-07-06 20:58:39 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 author 'Friskky'
 description 'Delivery Run for your criminal scums that want quick cash or just a simple delivery system'
-version '1.0.0'
+version '1.1.1'
 
 client_scripts {
 	'client/main.lua'
